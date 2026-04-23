c9cfc5c730e9e0963af70f545c4f1313217e9b66
Van Quy <vivanquy20804@gmail.com>
2026-04-23 19:26:19 +0700

    init project

---
+++ src/main/java/com/vanquy/cliniq/CliniqApplication.java
@@ -0,0 +1,13 @@
+package com.vanquy.cliniq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliniqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliniqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vanquy/cliniq/CliniqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanquy.cliniq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliniqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
